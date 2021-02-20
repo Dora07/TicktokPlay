e346d37e0c57fd8e6330da7e495e8ba863612b54
Dora <domonshieh@gmail.com>
2021-02-20 16:34:21 +0800

    Initial Commit

---
+++ TicktokPlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicktokPlay
+//
+//  Created by 謝孟芩 on 2021/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
